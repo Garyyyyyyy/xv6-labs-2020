384726f8ba9eeec9631c67b495449c3482e919b7
Adam Belay <adam@abelay.me>
2021-09-06 04:38:49 +0000

    util lab

---
+++ user/sleep.c
@@ -0,0 +1,17 @@
+// Gary 2023/6/21
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+
+    if(argc != 2){
+        fprintf(2, "Usage: rm files...\n");
+        exit(1);
+    }
+
+    int sleep_time = atoi(argv[1]);
+    sleep(sleep_time);
+    exit(0);
+}
